6ed001980eb27c56aa70f295f4b09990e3f3d977
Ferry Kobus <ferry@webmonkeys.nl>
2019-12-15 21:20:15 +0100

    Fix react example playground

--- playground/react/src/index.tsx
+++ playground/react/src/index.tsx
@@ -3,22 +3,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Other } from './Other';
 import './index.scss';
-import * as shit from 'formik-wizard';
-console.log(shit);
+
 class App extends Component {
   render() {
     const snapshot = { isDraggingOver: false };
-        function getSourceStyle(opts) {
-          return {};
-        }
+    function getSourceStyle(opts) {
+      return {};
+    }
 
-        return (
-          <div>
-            22222
-            <Other></Other>
-          </div>
-        );
-      }
+    return (
+      <div>
+        22222
+        <Other></Other>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
